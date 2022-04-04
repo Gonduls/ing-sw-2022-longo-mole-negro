8383b20532b511af17efd52de080ec64d037d028
Gonduls <pierluigi.negro.00@gmail.com>
2022-04-04 14:08:49 +0200

    fixed directories

--- src/main/java/it/polimi/ingsw/Test.java
+++ src/main/java/it/polimi/ingsw/Test.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, World!");
     }
 }
