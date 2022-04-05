fc72cc00713cae4ceff664a0dfd85d7c7be36315
gingervi <gingerogin@gmail.com>
2022-04-05 15:56:33 +0200

    implementation of Island and School

--- src/main/java/it/polimi/ingsw/model/Island.java
+++ src/main/java/it/polimi/ingsw/model/Island.java
@@ -1,4 +1,36 @@
 package it.polimi.ingsw.model;
 
 public class Island {
+
+    private int towerNumber;
+    private TowerColor towerColor;
+    private int noEntry; //we consider the possibility that a number of noEntries greater than 1 can be put on an Island
+
+    public int getTowerNumber() {
+        return towerNumber;
+    }
+
+    public TowerColor getTower() {
+        return towerColor;
+    }
+
+    public int getNoEntry() { //GameManager needs to check if there is a noEntry
+        return noEntry;
+    }
+
+    void addTower() {
+        towerNumber++;
+    }
+
+    void setTowerColor(TowerColor tower) {
+        towerColor = tower;
+    }
+
+    void addNoEntry() {
+        noEntry++;
+    }
+
+    void removeNoEntry() {
+        noEntry--;
+    }
 }
--- src/main/java/it/polimi/ingsw/model/School.java
+++ src/main/java/it/polimi/ingsw/model/School.java
@@ -6,26 +6,26 @@ public class School {
     private StudentHolder studentsAtEntrance;
 
     StudentHolder getStudentsAtTables() {
-
+        return studentsAtTables;
     }
 
     StudentHolder getStudentsAtEntrance() {
-
+        return studentsAtEntrance;
     }
 
-    void removeFromEntrance(Color student) {
-
+    void removeFromEntrance(Color student) throws noSuchStudentException {
+        studentsAtEntrance.removeStudent(student);
     }
 
-    void addToEntrance(Color student) {
-
+    void addToEntrance(Color student) throws noSpaceForStudentException {
+        studentsAtEntrance.addStudent(student);
     }
 
-    void removeFromTables(Color student) {
-
+    void removeFromTables(Color student) throws noSuchStudentException {
+        studentsAtTables.removeStudent(student);
     }
 
-    void addToTables(Color student) {
-
+    void addToTables(Color student) throws noSpaceForStudentException {
+        studentsAtTables.addStudent(student);
     }
 }
