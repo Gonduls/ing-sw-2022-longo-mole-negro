27e1fb9b952b14554c356df73ddb8cafa76bcea6
gingervi <gingerogin@gmail.com>
2022-04-05 15:57:09 +0200

    first implementation of Board

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -5,5 +5,25 @@ public class Board {
     private Island[] islands;
     private int motherNaturePosition;
 
+    public Island[] getIslands() {
+        return islands;
+    }
 
+    public int getMotherNaturePosition() {
+        return motherNaturePosition;
+    }
+
+    boolean canBeMerged(int index) {
+
+        return true;
+    }
+
+    void mergeIsland(int index) {
+
+    }
+
+    TowerColor calculateInfluence(int index) {
+        TowerColor tower = TowerColor.BLACK;
+        return tower;
+    }
 }
