7613f68c2e475eebaa904beb389824010c71edb4
gingervi <gingerogin@gmail.com>
2022-04-05 15:16:23 +0200

    filled class School

--- src/main/java/it/polimi/ingsw/model/School.java
+++ src/main/java/it/polimi/ingsw/model/School.java
@@ -1,4 +1,31 @@
 package it.polimi.ingsw.model;
 
 public class School {
+
+    private StudentHolder studentsAtTables;
+    private StudentHolder studentsAtEntrance;
+
+    StudentHolder getStudentsAtTables() {
+
+    }
+
+    StudentHolder getStudentsAtEntrance() {
+
+    }
+
+    void removeFromEntrance(Color student) {
+
+    }
+
+    void addToEntrance(Color student) {
+
+    }
+
+    void removeFromTables(Color student) {
+
+    }
+
+    void addToTables(Color student) {
+
+    }
 }
--- src/main/java/it/polimi/ingsw/model/TowerColor.java
+++ src/main/java/it/polimi/ingsw/model/TowerColor.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.model;
 
 public enum TowerColor {
-    BLACK, GREY, WHITE
+    BLACK,
+    GREY,
+    WHITE
 }
