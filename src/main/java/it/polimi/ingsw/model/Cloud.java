a3e6374ec59281d88443cca567941490143554af
Gonduls <pierluigi.negro.00@gmail.com>
2022-04-06 18:03:30 +0200

    finished cloud, corrected TablesObserver

--- src/main/java/it/polimi/ingsw/model/Cloud.java
+++ src/main/java/it/polimi/ingsw/model/Cloud.java
@@ -1,13 +1,32 @@
 package it.polimi.ingsw.model;
 
 public class Cloud extends StudentHolder{
+    final int size;
+    final Bag bag;
 
-    void moveAllStudents(School school) throws NoSpaceForStudentException, NoSuchStudentException{
+    public Cloud(int size, GameManager gameManager){
+        super(size, size);
+        this.size = size;
+        bag = gameManager.getBag();
+    }
+
+    void moveAllStudents(School school) throws NoSpaceForStudentException{
 
         for(Color c: Color.values()){
-
-            moveStudentTo(c, school.getStudentsAtEntrance());
+            for(int i = getStudentByColor(c); i>0; i--) {
+                try{
+                    moveStudentTo(c, school.getStudentsAtEntrance());
+                } catch(NoSuchStudentException e){
+                    System.out.println("Errors in finding students in cloud");
+                }
+            }
         }
 
+    }
+
+    void refill() throws NoSpaceForStudentException{
+        for(int i = 0; i< size; i++){
+            addStudent(bag.extractRandomStudent());
+        }
     }
 }
--- src/main/java/it/polimi/ingsw/model/TablesObserver.java
+++ src/main/java/it/polimi/ingsw/model/TablesObserver.java
@@ -3,14 +3,19 @@ package it.polimi.ingsw.model;
 public class TablesObserver implements StudentHolderObserver{
     private final StudentHolder observed;
     private final Player player;
+    private final Professors professors;
 
-    TablesObserver(Player player, StudentHolder observed){
+    TablesObserver(Player player, StudentHolder observed, GameManager gameManager){
         this.observed = observed;
         this.player = player;
+        professors = gameManager.getProfessors();
     }
 
     public void update(){
-        Professors professors = Professors.getInstance();
+        updateProfessors();
+    }
+
+    public void updateProfessors(){
         for(Color color : Color.values()){
             Player previousOwner = professors.getOwners().get(color);
 
