3b9e65eb1b413b348c843dd3e807c0e9b452c893
Gonduls <pierluigi.negro.00@gmail.com>
2022-04-16 20:05:50 +0200

    finished Player todo, added documentation for Player constructor

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,18 +1,32 @@
 package it.polimi.ingsw.model;
+
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+import java.util.List;
+
 public class Player {
 
     private final int playerNumber;
     private final String nickname;
     private int towersLeft;
     private final TowerColor towerColor;
-    private final AssistantCard[] deck;
+    private final List<AssistantCard> deck;
     private int coinsOwned;
     private final School school;
 
+    /**
+     * Builder method for Player, also instantiates School and deck associated to player.
+     * Given playerNumber and threePlayers, towersLeft and towerColor vary:
+     * 3 players -> 6 towers to each player, first towerColor to first player, second color to second player and so on
+     * !3 players -> 8 towers to player 1 and 2, first towerColor to player 1 (and 3), second towerColor to player 2 (and 4)
+     * @param playerNumber: used to distinguish players (and teams)
+     * @param nickname: player's unique identifier
+     * @param threePlayers: used identify a three players game
+     */
     public Player(int playerNumber, String nickname, boolean threePlayers){
         this.playerNumber = playerNumber;
         this.nickname = nickname;
-        deck = AssistantCard.values();
+        deck = Arrays.stream(AssistantCard.values()).toList();
         coinsOwned = 1;
 
         if(threePlayers) {
@@ -46,7 +60,7 @@ public class Player {
         return towerColor;
     }
 
-    public AssistantCard[] getCardsLeft() {
+    public List<AssistantCard> getCardsLeft() {
         return deck;
     }
 
@@ -55,19 +69,25 @@ public class Player {
     }
 
     public void setTowersNumber(int towers) {
-    //todo
+        towersLeft = towers;
     }
 
-    public void pickCard(AssistantCard card) {
-    //todo
+    public void pickCard(AssistantCard card) throws InvalidParameterException{
+        if(deck.contains(card))
+            deck.remove(card);
+        else
+            throw new InvalidParameterException("This card has already been used and is not in the deck anymore");
     }
 
     public void addCoin() {
-    //todo
+        coinsOwned ++;
     }
 
-    public void removeCoins(int coinsNumber) {
-    //todo
+    public void removeCoins(int coinsNumber) throws InvalidParameterException {
+        if(coinsNumber > coinsOwned)
+            throw new InvalidParameterException("There are not enough coins to execute this action");
+
+        coinsOwned -= coinsNumber;
     }
 
     public School getSchool(){
