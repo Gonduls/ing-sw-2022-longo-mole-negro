e2a2af4087290dcaac38df708d0e53424a091c15
gingervi <gingerogin@gmail.com>
2022-05-13 11:19:55 +0200

    added some methods

--- src/main/java/it/polimi/ingsw/client/ClientModelManager.java
+++ src/main/java/it/polimi/ingsw/client/ClientModelManager.java
@@ -12,6 +12,8 @@ public class ClientModelManager {
     Professors professors;
     List<Island> islands;
     List<Cloud> clouds;
+    StudentHolder[] characterCards;
+
 
     public ClientModelManager(int numberOfPlayers, boolean expert){
         players = new String[numberOfPlayers];
@@ -25,4 +27,30 @@ public class ClientModelManager {
         //view.update()
     }
     void addPlayer(String player, int position){}
+
+    void putSHInCharacterCard(int index0, int index1, int index2) {
+        this.characterCards[0] = CharacterCard.hasStudentHolder(index0) ? new StudentHolder() : null;
+        this.characterCards[1] = CharacterCard.hasStudentHolder(index1) ? new StudentHolder() : null;
+        this.characterCards[2] = CharacterCard.hasStudentHolder(index2) ? new StudentHolder() : null;
+    }
+
+    private StudentHolder parsePosition(String pos){
+        String[] splitPos = pos.split(":");
+        int index = Integer.parseInt(splitPos[1]);
+
+        switch (splitPos[0]) {
+            case("ISLAND"):
+                return islands.get(index);
+            case("CLOUD"):
+                return clouds.get(index);
+            case("ENTRANCE"):
+                return schools.get(index).getStudentsAtEntrance();
+            case("DININGROOM"):
+                return schools.get(index).getStudentsAtTables();
+            case("CHARACTERCARD"):
+                return characterCards[index];
+            default:
+                return null;
+        }
+    }
 }
--- src/main/java/it/polimi/ingsw/messages/AddStudentTo.java
+++ src/main/java/it/polimi/ingsw/messages/AddStudentTo.java
@@ -1,10 +1,27 @@
 package it.polimi.ingsw.messages;
 
+import it.polimi.ingsw.model.Color;
+
 public class AddStudentTo implements Message{
+    private final String where;
+    private final Color color;
 
     @Override
     public MessageType getMessageType() {
         return MessageType.ADD_STUDENT_TO;
+    }
+
+    public AddStudentTo(String where, Color color){
+        this.color = color;
+        this.where = where;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String getWhere() {
+        return where;
     }
 }
 
