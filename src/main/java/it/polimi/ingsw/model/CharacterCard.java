65ddc9192d0d811b708c1b844d402a38608b8f4c
Gonduls <pierluigi.negro.00@gmail.com>
2022-06-01 20:53:38 +0200

    Added Cards descriptions

--- src/main/java/it/polimi/ingsw/model/CharacterCard.java
+++ src/main/java/it/polimi/ingsw/model/CharacterCard.java
@@ -27,13 +27,21 @@ public abstract class CharacterCard {
     }
 
     public static String description(int id){
-        // todo: add all card descriptions
-        String response;
-        switch (id){
-            case 0 -> response = "generic card description";
-            default -> response = "even more generic card description";
-        }
-        return(response);
+        return switch (id){
+            case 0 -> "Take 1 student from this card and place it on an island of your choice";
+            case 1 -> "You can move Mother Nature up to 2 additional islands";
+            case 2 -> "You can exchange up to 3 students between this card and your entrance";
+            case 3 -> "You can exchange up to 2 students between your entrance and your dining room";
+            case 4 -> "During this turn, you take control of Professors even if you match the number of students of the previous owner";
+            case 5 -> "You can place a No Entry on a island. If Mother Nature ends on an island with a No Entry: remove one No Entry, no influence is calculated";
+            case 6 -> "During the influence calculation this turn, you count as having two more influence";
+            case 7 -> "Take one student from this card and place it in your dining room";
+            case 8 -> "Choose an island and resolve influence calculation as if Mother Nature had ended her turn there";
+            case 9 -> "This turn Towers do not count towards influence";
+            case 10 -> "Choose a color of a student: this turn students of that color do not count towards influence";
+            case 11 -> "Choose a color: every player (including yourself) must remove 3 (or all if they have less than 3) students of that color from their Dining Rooms";
+            default -> "No such card";
+        };
     }
 
     public  CharacterState getCharacterState(RoundController context, GameState nextState){
