6854dcc0bafe91a03d0ea5f766b8c536b5fd8628
Gonduls <pierluigi.negro.00@gmail.com>
2022-06-28 16:02:10 +0200

    added javafx to pom, now able to run from windows, corrected PublicRooms

--- src/main/java/it/polimi/ingsw/messages/PublicRooms.java
+++ src/main/java/it/polimi/ingsw/messages/PublicRooms.java
@@ -2,13 +2,14 @@ package it.polimi.ingsw.messages;
 
 import it.polimi.ingsw.server.RoomInfo;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * Message that holds all public rooms available that fitted the previous request
  * @param rooms The info requested
  */
-public record PublicRooms(List<RoomInfo> rooms) implements Message {
+public record PublicRooms(List<RoomInfo> rooms) implements Message, Serializable {
 
     /**
      * @return The MessageType of this message (PUBLIC_ROOMS)
