5f6c6c5f458d22aee8396d841e0b84745ef674f5
gingervi <gingerogin@gmail.com>
2022-06-30 13:29:40 +0200

    reset fullscreen

--- src/main/java/it/polimi/ingsw/client/view/gui/GUI.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/GUI.java
@@ -83,7 +83,7 @@ public class GUI extends Application implements UI{
                     root = FXMLLoader.load(getClass().getResource("/fxml/UpdatedGameBoard.fxml"));
                     scene = new Scene(root, 1366, 768);
                     primaryStage.setScene(scene);
-                    primaryStage.setFullScreen(false);
+                    primaryStage.setFullScreen(true);
                     primaryStage.setResizable(false);
                     primaryStage.setHeight(768);
                     primaryStage.setWidth(1366);
